d24613dac10cc56b729fb755403aaa70ccbe131d
phatdroid94 <phatdroid94@gmail.com>
2020-06-01 21:07:01 +0700

    single choice

--- app/src/main/java/com/example/alertdialog30032020/MainActivity.java
+++ app/src/main/java/com/example/alertdialog30032020/MainActivity.java
@@ -24,30 +24,19 @@ public class MainActivity extends AppCompatActivity {
         mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String[] arrayAnimals = {"Meo","Ca","Voi","Heo","Ngua"};
                 AlertDialog.Builder builder =
                         new AlertDialog.Builder(MainActivity.this)
                                 .setTitle("Có tin thông báo mới")
-                                .setMessage("Có phiên bản cập nhật")
                                 .setIcon(R.mipmap.ic_launcher)
                                 .setCancelable(false)
-                                .setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                                .setSingleChoiceItems(arrayAnimals, -1, new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
-                                        Toast.makeText(MainActivity.this, "Nút có", Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .setNegativeButton("Không", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Toast.makeText(MainActivity.this, "Nút không", Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Toast.makeText(MainActivity.this, "Nút hủy", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(MainActivity.this, arrayAnimals[which], Toast.LENGTH_SHORT).show();
                                     }
                                 });
+
                 builder.show();
 
             }
